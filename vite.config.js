ab8966b9d0612766d792b0af130e27daedab20ab
bunyol15 <149569878+bunyol15@users.noreply.github.com>
2025-08-30 20:05:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/to-twist01/', // Muy importante: debe ser el nombre EXACTO de tu repo entre slashes
+  base: '/to-twist0/', // Muy importante: debe ser el nombre EXACTO de tu repo entre slashes
 })
